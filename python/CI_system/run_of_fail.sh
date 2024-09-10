e3ad7e80918cf253d3246d3660f3eef2e6b57f1e
achiefa <amedeochiefa@gmail.com>
2024-09-10 20:24:27 +0200

    Saving progress

--- python/CI_system/run_of_fail.sh
+++ python/CI_system/run_of_fail.sh
@@ -2,10 +2,13 @@
 
 run_or_fail() {
   local explanation=$1 # local scope
-  shift 1
+  shift 1 # shift args to the left by 1
   "$@"
   if [ $? != 0 ]; then # `$?` holds the exit status of the last command executed.
     echo $explanation 1>&2 # redirect the standard output (stdout) to the standard error (stderr)
+    # 1 is the file descriptor for standard output (stdput)
+    # 2 is the file descriptor for standard error (stderr). The `&` before 
+    # indicates that `2` is a file file descriptor and not a file name
     exit 1
   fi
 }
--- python/CI_system/update_repo.sh
+++ python/CI_system/update_repo.sh
@@ -5,3 +5,4 @@ source run_or_fail.sh
 # delete previous version id
 rm -f .commit_id
 
+run_or_fail "Repository folder not found!" pushd $1 1> /dev/null
